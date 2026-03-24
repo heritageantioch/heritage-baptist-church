86c1acdb0e4860fe2722c6460da3796cdf15168e
Heritage Baptist <heritageantioch@gmail.com>
2026-03-24 15:24:27 -0700

    Update spacing between Watch Sermons and Give Online links in Footer.tsx

---
+++ Footer.tsx
@@ -0,0 +1,18 @@
+// Updated Footer.tsx content
+
+import React from 'react';
+
+const Footer = () => {
+    return (
+        <footer className="bg-gray-800 text-white py-4">
+            <div className="container mx-auto flex flex-col items-center">
+                <div className="space-y-8">
+                    <a href="#" className="text-lg">Watch Sermons</a>
+                    <a href="#" className="text-lg">Give Online</a>
+                </div>
+            </div>
+        </footer>
+    );
+};
+
+export default Footer;
